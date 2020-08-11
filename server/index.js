3ffae75c804d73eca87463478bc46710f7c02ff2
Noal Brown <noalbrown1@gmail.com>
2020-08-11 15:23:00 -0500

    node_4_afternoon

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const session = require("express-session");
 const checkForSession = require('./middlewares/checkForSession');
 const swagController = require("./controllers/swagController");
+const authController = require('./controllers/authController');
+const cartController = require("./controllers/cartController");
+const searchController = require("./controllers/searchController");
 
 const app = express();
 
@@ -18,8 +21,20 @@ app.use(
 );
 
 app.use(checkForSession);
+app.use(express.static(`${__dirname}/../build`));
+
+app.post("/api/register", authController.register);
+app.post("/api/login", authController.login);
+app.post("/api/signout", authController.signout);
+app.get("/api/user", authController.getUser);
 
 app.get("/api/swag", swagController.read);
+
+app.post("/api/cart/checkout", cartController.checkout);
+app.post("/api/cart/:id", cartController.add);
+app.delete("/api/cart/:id", cartController.delete);
+
+app.get("/api/search", searchController.search);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}.`);
